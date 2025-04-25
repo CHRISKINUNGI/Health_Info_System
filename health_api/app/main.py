cb5a1ed0e5595630ffbd7e45dbe117e9a8c11513
CHRISKINUNGI <chrskinungi@gmail.com>
2025-04-26 02:16:46 +0300

    refactor(api): add enrollments route to the existing main.py
    - Integrated the enrollments API route into the main FastAPI app.
    - Added the  endpoint under the Enrollments tag.
    - Ensured the enrollments route works alongside the existing programs and clients routes.

--- health_api/app/main.py
+++ health_api/app/main.py
@@ -2,12 +2,13 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.programs import api as programs_api
 from app.clients import api as clients_api
+from app.enrollments import api as enrollments_api
 
 def create_app():
     app = FastAPI(
         title="Basic Health Information System API",
         description="API for managing health programs.",
-        version="0.2.0",
+        version="0.3.0",
         openapi_url="/api/v1/openapi.json",
         docs_url="/api/v1/docs",
         redoc_url="/api/v1/redoc",
@@ -23,6 +24,7 @@ def create_app():
 
     app.include_router(programs_api.router, prefix="/api/v1", tags=["Programs"])
     app.include_router(clients_api.router, prefix="/api/v1", tags=["Clients"])
+    app.include_router(enrollments_api.router, prefix="/api/v1", tags=["Enrollments"])
 
     return app
 
