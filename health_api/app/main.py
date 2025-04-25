99ab5615ee14f48c7e8f98fde4ac53c03ae0ebcf
CHRISKINUNGI <chrskinungi@gmail.com>
2025-04-25 23:35:11 +0300

    refactor(api): update main.py to include clients routes
    - Refactored  to add routes for managing clients via the  module.
    - Included client-related endpoints under the  prefix.
    - Updated FastAPI app configuration to include the new client routes.

--- health_api/app/main.py
+++ health_api/app/main.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from .programs import api as programs_api
+from app.programs import api as programs_api
+from app.clients import api as clients_api
 
 def create_app():
     app = FastAPI(
         title="Basic Health Information System API",
         description="API for managing health programs.",
-        version="0.1.0",
+        version="0.2.0",
         openapi_url="/api/v1/openapi.json",
         docs_url="/api/v1/docs",
         redoc_url="/api/v1/redoc",
@@ -21,6 +22,7 @@ def create_app():
     )
 
     app.include_router(programs_api.router, prefix="/api/v1", tags=["Programs"])
+    app.include_router(clients_api.router, prefix="/api/v1", tags=["Clients"])
 
     return app
 
